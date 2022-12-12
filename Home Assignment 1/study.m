2db04e1ee044d1d66eb8d2a9a93d2aa7d16977ad
jason liu <jasonliu@163.com>
2022-12-12 22:38:40 +0800

    作业二第一题完成

--- Home Assignment 1/study.m
+++ Home Assignment 1/study.m
@@ -35,16 +35,6 @@ hold on
 legend('3\sigma_{affine}',"3\sigma_{approx}",'Samples','mean_{affine}','mean_{approx}','location','northeast')
 hold off
 
-% NonlinearTransform = @(x)[sqrt(x(1,:).^2 + x(2,:).^2);atan2(x(2,:),x(1,:))];
-% [mu_approx_Nonlinear, Sigma_approx_Nonlinear, Gaussian_approx_Nonlinear] = approxGaussianTransform(mu_x, Sigma_x, NonlinearTransform, 5000);
-% xy_approx_3_sigma_Nonlinear = sigmaEllipse2D(mu_approx_Nonlinear, Sigma_approx_Nonlinear, 3);
-% figure;
-% plot(xy_approx_3_sigma_Nonlinear(1,:), xy_approx_3_sigma_Nonlinear(2,:),"Color","black","LineWidth",2)
-% axis equal
-% hold on 
-% plot(Gaussian_approx_Nonlinear(1,:), Gaussian_approx_Nonlinear(2,:),'.',"Color","red","LineWidth",2)
-% axis equal
-
 NonlinearTransform = @(x)[sqrt(x(1,:).^2 + x(2,:).^2);atan2(x(2,:),x(1,:))];
 
 [mu_approx_nonlinear, Sigma_approx_nonlinear, Gaussian_approx_nonlinear] = approxGaussianTransform(mu_x, Sigma_x, NonlinearTransform, 5000);
@@ -63,5 +53,44 @@ axis equal
 hold on
 legend('3\sigma_{approx}','mean_{approx}','Samples','location','northeast')
 hold off
+
+% %%Problem 2: 挪威雪的深度
+Hafjell_mu_x = 1.1;
+Hafjell_sigma2_x = 0.5.^2;
+Anna_sigma2_r = 0.2.^2;
+[Hafjell_mu, Hafjell_Sigma]= jointGaussian(Hafjell_mu_x, Hafjell_sigma2_x, Anna_sigma2_r);
+
+mu_x = 1.0;
+sigma2_x = 0.5.^2;
+sigma2_r = 1.^2;
+[mu, Sigma]= jointGaussian(mu_x, sigma2_x, sigma2_r);
+
+xy_Hafjell_3_sigma = sigmaEllipse2D(Hafjell_mu, Hafjell_Sigma, 3)
+xy_sigma = sigmaEllipse2D(mu, Sigma, 3)
+data_points = mvnrnd(Hafjell_mu, Hafjell_Sigma, 5000);
+data_point2s = mvnrnd(mu, Sigma, 5000);
+figure;
+plot(xy_Hafjell_3_sigma(1,:), xy_Hafjell_3_sigma(2,:),"Color","blue","LineWidth",2)
+axis equal
+hold on
+plot(data_points(:,1),data_points(:,2),".","Color","red","LineWidth",2)
+axis equal
+hold on
+plot(xy_sigma(1,:), xy_sigma(2,:),"Color","blue","LineWidth",2)
+axis equal
+hold on
+plot(data_point2s(:,1),data_point2s(:,2),".","Color","black","LineWidth",2)
+axis equal
+hold off
+
+
+
+
+
+
+
+
+
+
 
 
